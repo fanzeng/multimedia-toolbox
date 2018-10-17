f2d8c8457fc1a35310d08e10730b1e7d611e5be1
FanZeng <fanzengau@gmail.com>
2018-10-17 18:11:10 +1000

    added frame-preview module

--- fanzeng.co.nf/apps/video_creator/video_creator.php
+++ fanzeng.co.nf/apps/video_creator/video_creator.php
@@ -4,17 +4,28 @@ session_start();
 	ini_set('display_errors', 1);
 	ini_set('display_startup_errors', 1);
 	error_reporting(E_ALL);
+	$user_data_dir = "../../data/user_data";
 	if(!isset($_SESSION["session_initialised"])) {
+
 		$hash = bin2hex(random_bytes(16));
 		$uniqid = uniqid();
 		$_SESSION["user_id"] = $uniqid; 
-		exec("mkdir -p ./data/" . $uniqid . "/source_images/ 2>&1", $std_out);
+		exec("mkdir -p $user_data_dir/" . $uniqid . "/source_images/ 2>&1", $std_out);
+		foreach($std_out as $out) {
+			echo $out;
+		}
+		exec("mkdir -p $user_data_dir/" . $uniqid . "/expanded_frames/ 2>&1", $std_out);
+
 	    $_SESSION["session_initialised"] = 0;
-	    $_SESSION["frame_id"] = 0;
+	    $_SESSION["source_image_num"] = 0;
+	    $_SESSION["array_source_file_names"] = array();
+	    $_SESSION["array_number_of_frames"] = array();
+
 	}
+
 	if (isset($_POST["start_over"])) {
 		$std_out = array();
-		echo exec("rm -rf ./data/" . $_SESSION["user_id"] . "/* 2>&1", $std_out);
+		echo exec("rm -rf $user_data_dir/" . $_SESSION["user_id"] . "/* 2>&1", $std_out);
 		if (empty($std_out)) {
 			complete_session_destroy();
 			echo "All your files have been deleted.";
@@ -34,62 +45,94 @@ session_start();
 		echo "Welcome.<br>";
 		echo  "Your user id is: <br> " . $_SESSION["user_id"] . '<br>';
 	
-		exec('ls ./data/' . $_SESSION["user_id"] . '/source_images/*.morph.* | wc -l 2>&1', $std_out);
+		exec("ls $user_data_dir/" . $_SESSION["user_id"] . '/source_images/* | wc -l 2>&1', $std_out);
 		echo "Existing number of frames: " . $std_out[0] . "<br>";
 
 		if(isset($_POST["number_of_frames"])) {
-			if ($_POST["start_frame_number"] != 0) {
-				$_SESSION["frame_id"] = (int)$_POST["start_frame_number"];
-			}
-			foreach($_FILES["files_to_upload"]["tmp_name"] as $key=>$tmp_name){
-		    	$number_of_frames = $_POST["number_of_frames"];
-		    	$temp_name = $_FILES["files_to_upload"]["tmp_name"][$key];
-		    	$name = $_FILES["files_to_upload"]["name"][$key];
-		    	$ext = pathinfo($name, PATHINFO_EXTENSION);
-				check_uploaded_file();
-				$upload_success = True;
-				$temp_target_file = "./data/" . $_SESSION["user_id"]  . "/source_images/temp_target_file." . $ext ;
-				if (move_uploaded_file($temp_name, $temp_target_file) != True ) {
-					$upload_sucess = False;
-				}
-				for ($frame_num = 0; $frame_num < $number_of_frames; $frame_num++) {
-					$target_file_name = get_target_file_name($ext);
-					exec("cp " . $temp_target_file . " " . $target_file_name, $std_out);
-					$frame_num_submitted = $frame_num;
-				}
-				$frame_num_submitted = $frame_num;
-		   		if ($upload_success) {
-		   			echo "All good. <br>";
-			    } else {
-			        echo "[WARN] Sorry, there was an error uploading your file.";
-			    }
-		       	echo "[INFO] " . $frame_num_submitted . " frame(s) of " . basename($name). " has been uploaded. <br>";
-
-			}
+			upload_images();
 		} else if (isset($_POST["make_video"])) {
-			if(isset($_SESSION["session_initialised"])) {
-				$sh_string = 'video_width=' . $_POST["video_width"] . ' && ';
-				$sh_string = $sh_string . 'video_height=' . $_POST["video_height"] . ' && ';
-				$sh_string = $sh_string . 'frames_per_second=' . $_POST["frames_per_second"] . ' && ';
-				$sh_string = $sh_string . 'quality_ratio=' . $_POST["quality_ratio"] . ' && ';
-				$sh_string = $sh_string . 'dirname=' . $_SESSION["user_id"] . ' && ';
-				$sh_string = $sh_string . 'echo dirname=\$dirname && ';
-				$sh_string = $sh_string . '/usr/bin/ffmpeg -y -i ./data/\$dirname/source_images/"%05d.morph.jpg' . '" -r "\$frames_per_second" -crf \"\$quality_ratio\" -s \"\$video_width\"x\"\$video_height\"  ./data/"\$dirname"/output.mp4';
-
-				exec('echo "' . $sh_string  . ' "> ' . './data/' . $_SESSION["user_id"] . '/run.sh');
-				exec("chmod +x ./data/" . $_SESSION["user_id"] . "/run.sh", $std_out);
-				echo exec("./data/" . $_SESSION["user_id"] . "/run.sh 2>&1", $std_out);
-				foreach($std_out as $out) {
-					echo $out;
-				}
-				echo '<h1>Download the video here (right click, save as): <a href="./data/' .  $_SESSION["user_id"] . '/output.mp4"' . '>output.mp4</a></h1><br>';
-			} else {
-				echo "Please click Start Over.";
-			}
+			make_video();
 		} else {
 			echo "No work to do.<br>";
 		}
 	}
+
+	function update_json() {
+		global $user_data_dir;
+	    $json_file = "$user_data_dir/" . $_SESSION["user_id"] . "/frames.json";
+
+		$handle = fopen($json_file, 'w') or die('Cannot open file:  '.$json_file);
+		$json_data = json_encode($_SESSION);
+		echo $json_data;
+		fwrite($handle, $json_data);
+	}
+
+
+	function upload_images() {
+		// if ($_POST["start_frame_number"] != 0) {
+		// 	$_SESSION["frame_id"] = (int)$_POST["start_frame_number"];
+		// }
+		global $user_data_dir;
+		foreach($_FILES["files_to_upload"]["tmp_name"] as $key=>$tmp_name){
+	    	$number_of_frames = (int)$_POST["number_of_frames"];
+	    	$temp_name = $_FILES["files_to_upload"]["tmp_name"][$key];
+	    	$name = $_FILES["files_to_upload"]["name"][$key];
+	    	$ext = pathinfo($name, PATHINFO_EXTENSION);
+			check_uploaded_file();
+			$upload_success = True;
+			$source_file_name = "$user_data_dir/" . $_SESSION["user_id"]  . "/source_images/source_image_" . (string)$_SESSION["source_image_num"] . "." . $ext ;
+			echo $source_file_name;
+			if (move_uploaded_file($temp_name, $source_file_name) != True ) {
+				$upload_sucess = False;
+				echo "Failed to upload the file(s).";
+			} else {
+				$_SESSION["source_image_num"]++;
+				array_push($_SESSION["array_source_file_names"], $source_file_name);
+				array_push($_SESSION["array_number_of_frames"], $number_of_frames);
+				update_json();
+			}
+
+		}
+	}
+
+
+	function expand_frames() {
+		$_SESSION["frame_id"] = 0;
+		for ($source_image_num = 0; $source_image_num < $_SESSION["source_image_num"]; $source_image_num++) {
+			for($repeat_num = 0; $repeat_num < $_SESSION["array_number_of_frames"][$source_image_num]; $repeat_num++) {
+				$source_file_name = $_SESSION["array_source_file_names"][$source_image_num];
+			   	$ext = pathinfo($source_file_name, PATHINFO_EXTENSION);
+				$target_file_name = get_target_file_name($ext);
+			exec("cp " . $source_file_name . " " . $target_file_name, $std_out);
+			}
+ 		}
+	}
+
+	function make_video() {
+		global $user_data_dir;
+
+		if(isset($_SESSION["session_initialised"])) {
+			expand_frames();
+			$sh_string = 'video_width=' . $_POST["video_width"] . ' && ';
+			$sh_string = $sh_string . 'video_height=' . $_POST["video_height"] . ' && ';
+			$sh_string = $sh_string . 'frames_per_second=' . $_POST["frames_per_second"] . ' && ';
+			$sh_string = $sh_string . 'quality_ratio=' . $_POST["quality_ratio"] . ' && ';
+			$sh_string = $sh_string . 'dirname=' . $_SESSION["user_id"] . ' && ';
+			$sh_string = $sh_string . 'echo dirname=\$dirname && ';
+			$sh_string = $sh_string . '/usr/bin/ffmpeg -y -i ' . $user_data_dir . '/\$dirname/expanded_frames/%05d.morph.jpg' . ' -r "\$frames_per_second" -crf \"\$quality_ratio\" -s \"\$video_width\"x\"\$video_height\" ' . $user_data_dir . '/"\$dirname"/output.mp4';
+
+			exec('echo "' . $sh_string  . ' "> ' . "$user_data_dir/" . $_SESSION["user_id"] . '/run.sh');
+			exec("chmod +x $user_data_dir/" . $_SESSION["user_id"] . "/run.sh", $std_out);
+			echo exec("$user_data_dir/" . $_SESSION["user_id"] . "/run.sh 2>&1", $std_out);
+			foreach($std_out as $out) {
+				echo $out;
+			}
+			echo '<h1>Download the video here (right click, save as): <a href="$user_data_dir/' .  $_SESSION["user_id"] . '/output.mp4"' . '>output.mp4</a></h1><br>';
+		} else {
+			echo "Please click Start Over.";
+		}
+	}
+
 	function check_uploaded_file() {
 		$is_good_image = 1;
 		// $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
@@ -105,16 +148,13 @@ session_start();
 	}
 
 	function get_target_file_name($ext) {
-		$target_dir = "./data/" . $_SESSION["user_id"] . "/source_images/";
+		global $user_data_dir;
+		$target_dir = "$user_data_dir/" . $_SESSION["user_id"] . "/expanded_frames/";
 		$frame_id_str = sprintf('%05d', $_SESSION["frame_id"]) ;
 		$target_file_name = $target_dir . $frame_id_str . ".morph." . $ext;
-		// echo "[INFO] Uploading file: " . $target_file_name . " . ";
 		$_SESSION["frame_id"] = (int)$_SESSION["frame_id"] + 1;
-		// echo "[INFO] Submitted " . $_SESSION["frame_id"] . " frames.<br>";
 		return $target_file_name;
 	}
-
-
 
 	function complete_session_destroy() {
 		unset($_SESSION["session_initialised"]);
@@ -128,13 +168,18 @@ session_start();
 	}
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" ng-app="framePreviewApp">
 <head>
 	<title>Online Video Creator</title>
 	<link rel="stylesheet" type="text/css" href="../../css/default.css">
 	<script type="text/javascript" src="../../vendors/scripts/jquery.min.js"></script>
 	<script type="text/javascript" src="../../vendors/scripts/angular.min.js"></script>
 	<script type="text/javascript" src="scripts/videoCreator.js"></script>
+    <script type="text/javascript" src="scripts/app.module.js"></script>
+    <script type="text/javascript" src="scripts/frame-preview/frame-preview.module.js"></script>
+    <script type="text/javascript" src="scripts/frame-preview/frame-preview.component.js"></script>
+
+
 </head>
 <body>
 	<br>Disclaimer: Your images will be deleted after use. But keep in mind this is on external web hosting. Please use with discretions. <br><br>
@@ -154,7 +199,7 @@ session_start();
 
 
 
-	<!-- <frame-preview></frame-preview> -->
+	<frame-preview></frame-preview>
 
 	Step 2: Specify video parameters:<br>
 	<form action="#" method="post" enctype="multipart/form-data">
