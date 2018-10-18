c005aa0cbfad2518c10700ca1f4b05ea069c1271
FanZeng <fanzengau@gmail.com>
2018-10-18 17:51:31 +1000

    added frame-preview image container

--- fanzeng.co.nf/apps/video_creator/scripts/frame-preview/frame-preview.component.js
+++ fanzeng.co.nf/apps/video_creator/scripts/frame-preview/frame-preview.component.js
@@ -4,10 +4,10 @@ angular.
   module('framePreview').
   component('framePreview', {
     templateUrl: 'scripts/frame-preview/frame-preview.template.html',
-    controller: function framePreviewController($http) {
+    controller: ['$http', function framePreviewController($http) {
       var self = this;
       $http.get('get_frames_json.php').then(function(response) {
         self.frames = response.data;
       });
     }
-  });
+  ]});
