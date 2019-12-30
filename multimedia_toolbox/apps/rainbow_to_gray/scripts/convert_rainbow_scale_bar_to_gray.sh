e0932535499c5af38cca851ef7812ef2a44e1bc6
Fan Zeng <fanzengau@gmail.com>
2019-12-30 12:48:00 +1000

    remove duplicated files

--- multimedia_toolbox/apps/rainbow_to_gray/scripts/convert_rainbow_scale_bar_to_gray.sh
+++
@@ -1,6 +0,0 @@
-# python ./convert_rainbow_scale_bar_to_gray.py --in_image_file ../../../data/site_data/rainbow_to_gray/tests/test_0/0.png \
-# --out_path ../../../data/site_data/rainbow_to_gray/tests/test_0/ --rainbow_position 390 \
-# --dark_end 430 --bright_end 940 -is_full_sat
-python ./convert_rainbow_scale_bar_to_gray.py --in_image_file ../../../data/site_data/rainbow_to_gray/tests/test_2/2.png \
---out_path ../../../data/site_data/rainbow_to_gray/tests/test_2/ --rainbow_position 221 \
---dark_end 56 --bright_end 538 -is_vertical
