97556dbf9ce2284d53498ba1bbba60d808e12cbd
Fan Zeng <fanzengau@gmail.com>
2023-01-22 14:14:38 +1000

    update setup.sh to include php7.2 install on Ubuntu 22.04

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,3 +1,12 @@
+# Install php7.2
+sudo LC_ALL=C.UTF-8 add-apt-repository ppa:ondrej/php
+sudo apt update
+sudo apt install php7.2
+
+# Create the bin directory
+cd server
+mkdir bin
+
 # Setup composer
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php -r "if (hash_file('sha384', 'composer-setup.php') === '55ce33d7678c5a611085589f1f3ddf8b3c52d662cd01d4ba75c0ee0459970c2200a51f492d557530c71c15d8dba01eae') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
